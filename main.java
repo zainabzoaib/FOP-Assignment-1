ba72080545b8937f713c2235e053c207fa4e4e3c
zainabzoaib <zainab.zoaib@gmail.com>
2022-03-15 23:19:27 +1100

    Update main.java
    making conditional changes and adding array list

--- main.java
+++ main.java
@@ -6,45 +6,53 @@ import java.util.ArrayList;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class main
-{
-    // instance variables - replace the example below with your own
-    private String unitName;
-    private int stdMarks;
-    
-    /**
-     * Constructor for objects of class main
-     */
-    public main()
-    {
-        // initialise instance variables
-        getUnitName();
-        getMarks();
-    }
-    public void getUnitName(){
-         System.out.println("Please type the unit name");
-         unitName=((new Scanner(System.in).next()));
-    }
-    
-    public void getMarks(){
-        for(int i=1;i<=25;i++){
-        boolean correctInput=false;
-        do{
-         System.out.println("Please type the students marks");
-         stdMarks=((new Scanner(System.in).nextInt()));
-         if(stdMarks>0 && stdMarks<100){
-            correctInput=true;
-            }else{
-                System.out.println("Incorrect value, please type a number between 0 and 100");
-            }
-             }while(!correctInput);   
-             
-        }
-        }
-        
-    public static void main(String[] args){
-        main myObj=new main();
-    }
-    
-}
+public class main {
+  // instance variables - replace the example below with your own
+  private String unitName;
+  private ArrayList < Double > stdMarks = new ArrayList < Double > ();
 
+  /**
+   * Constructor for objects of class main
+   */
+  public main() {
+    // initialise instance variables
+    getUnitName();
+    getMarks();
+    findMinandMax();
+  }
+  public void getUnitName() {
+    System.out.println("Please type the unit name");
+    unitName = ((new Scanner(System.in).next()));
+  }
+
+  public void getMarks() {
+    boolean numberInput = false;
+    int i = 1;
+    do {
+      System.out.println("Please type the students marks");
+      double marks = new Scanner(System.in).nextDouble();
+      if (marks > 0 && marks <= 100) {
+        stdMarks.add(marks);
+        i++;
+
+      } else {
+        System.out.println("Incorrect value, please type a number between 0 and 100");
+        numberInput = false;
+        i--;
+
+      }
+    }
+    while (!numberInput && i <= 25);
+    System.out.println(unitName.toString());
+    System.out.println(stdMarks.toString());
+  }
+
+  public void findMinandMax() {
+
+  }
+
+  public static void main(String[] args) {
+    main myObj = new main();
+  }
+
+}
